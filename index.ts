07717e9172648e12f50de96e1e1a137ec8616a7d
diegommezp28 <da.gomezp@uniandes.edu.co>
2019-04-03 20:47:09 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,6 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let elemento = document.createElement("table");
+elemento.innerHTML = '<tr> <td > <div class="col-10" style="background-color:lightgreen;">nnnnnnnnnnnnnnnnnnnnn</div> </td></tr>';
+appDiv.appendChild(elemento);
